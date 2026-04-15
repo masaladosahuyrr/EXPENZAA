325df5b53ad7142adedf123128cc80808a6b3443
yuviixid <snghyuraj@gmail.com>
2026-04-15 12:54:21 +0530

    added login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Login clicked", email, password);
+    // later connect Firebase login here
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
